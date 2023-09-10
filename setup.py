a2846b7e55ffadbb7904cf773b6f01a8af595cd8
Douile <25043847+Douile@users.noreply.github.com>
2023-09-10 22:05:05 +0100

    setup: Get requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python3
 
+import re
 from setuptools import setup
+
+REQ_PATTERN = re.compile(
+    "(?P<name>[^=<>]+)(?P<comp>[<=>]{1,2})(?P<spec>[^;]+)"
+    "(?:(;\W*python_version\W*(?P<pycomp>[<=>]{1,2})\W*"
+    "(?P<pyspec>[0-9\.]+)))?"
+)
+
+
+# Taken from:
+# https://github.com/sagivo/zipline/blob/18aba63da968ea1b913ed049ff14685eb3830f5b/setup.py#L119
+def _filter_requirements(lines_iter):
+    for line in lines_iter:
+        line = line.strip()
+        if not line or line.startswith("#"):
+            continue
+
+        match = REQ_PATTERN.match(line)
+        if match is None:
+            raise AssertionError("Could not parse requirement: '%s'" % line)
+
+        yield line
+
 
 with open("README.md", "r") as f:
     long_description = f.read()
+
+with open("requirements.txt", "r") as f:
+    install_requires = list(_filter_requirements(f.readlines()))
 
 setup(
     name="friends-queue",
@@ -15,7 +41,7 @@ setup(
     author_email="douile@douile.com",
     url="https://github.com/Douile/friends-queue",
     packages=["friends_queue"],
-    package_data={"":["*.css"]},
+    package_data={"": ["*.css"]},
     include_package_data=True,
-    install_requires=["mpv", "yt-dlp"],
+    install_requires=install_requires,
 )
