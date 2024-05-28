fbc1440a05d4644c48863ea48b987c9df63d7768
Douile <25043847+Douile@users.noreply.github.com>
2024-05-28 20:11:48 +0100

    Fix time display when dragging seekbar
    Closes #19

--- friends_queue/friends_queue.js
+++ friends_queue/friends_queue.js
@@ -13,8 +13,17 @@ function getTimeElms() {
   return [timeBefore, timeAfter];
 }
 
+const numberFormatter = new Intl.NumberFormat(undefined, {
+  style: "decimal",
+  minimumIntegerDigits: 2,
+  maximumFractionDigits: 0,
+});
 function durationToStr(duration) {
-  return new Date(duration * 1000).toTimeString().split(" ")[0];
+  const seconds = duration % 60;
+  let minutes = duration / 60;
+  const hours = minutes / 60;
+  minutes = minutes % 60;
+  return `${numberFormatter.format(hours)}:${numberFormatter.format(minutes)}:${numberFormatter.format(seconds)}`;
 }
 
 function updateSeekTimes(input) {
